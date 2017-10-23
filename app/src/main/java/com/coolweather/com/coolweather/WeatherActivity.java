3cfd743f4e6de77a3a50e27fc5a31e4b4c9f3c5d
zjy <18577081926@163.com>
2017-10-23 17:09:44 +0800

    增加了后台自动更新天气功能

--- app/src/main/java/com/coolweather/com/coolweather/WeatherActivity.java
+++ app/src/main/java/com/coolweather/com/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.com.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.com.coolweather.gson.Forecast;
 import com.coolweather.com.coolweather.gson.Weather;
+import com.coolweather.com.coolweather.service.AutoUpdateService;
 import com.coolweather.com.coolweather.util.HttpUtil;
 import com.coolweather.com.coolweather.util.Utility;
 
@@ -149,6 +151,8 @@ public class WeatherActivity extends AppCompatActivity {
                             editor.apply();
                            // weatherId = weather.basic.weatherId;
                             showWeatherInfo(weather);
+
+
                         }else {
                             Toast.makeText(WeatherActivity.this,"获取天气信息失败",
                                     Toast.LENGTH_SHORT).show();
@@ -198,6 +202,9 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
+
     }
 
 
